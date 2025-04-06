6e60b0bfc2bb269a22993a2247f895ee305ee3e5
Virendra Singh <virendrasingh.v12@gmail.com>
2025-04-06 14:10:31 +0530

    fresh

---
+++ string.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+    public class string {
+
+ 
+    public static void main(String[] var0) {
+       Scanner sc  = new Scanner(System.in);
+       System.out.println("ente your name");
+       String name = sc.nextLine();
+       System.out.println("Hello " + name + " Have a good day");
+       System.out.println(sc.nextInt());
+    }
+ }
